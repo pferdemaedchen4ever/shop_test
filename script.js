3d901813daa90ca47c61167f620bd357e26cd05e
Rick Kunze <104360509+pferdemaedchen4ever@users.noreply.github.com>
2023-07-11 15:01:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const cards = document.querySelectorAll('.card');
-  
-    cards.forEach(function (card) {
-      card.addEventListener('click', function () {
-        this.classList.toggle('clicked');
-      });
+  const heading = document.querySelector('h1');
+  setTimeout(function () {
+    heading.classList.add('fade-in');
+  }, 500);
+
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach(function (card) {
+    card.addEventListener('click', function () {
+      this.classList.toggle('clicked');
     });
   });
-  
+});
